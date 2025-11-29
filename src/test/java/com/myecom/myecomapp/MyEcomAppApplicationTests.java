6c8f55b966977e1b6029e77a40003b4a34cd966a
Divyank Rajput <rajputdivyanking@gmail.com>
2025-11-29 23:00:00 +0530

    Add Maven wrapper, pom.xml, project file, and test folder

---
+++ src/test/java/com/myecom/myecomapp/MyEcomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myecom.myecomapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEcomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
